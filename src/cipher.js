c26e16832fac2f811c8ce04ca5e261064df9fe5c
albertoouu <albertoouu@gmail.com>
2021-10-04 18:59:06 -0500

    refactorizacion funciones code y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,90 @@
+
 window.cipher = {
-  saludar: function saludar () {
+  code: function saludar () {
     console.log("Hello")
   },
   name: "beto"
 };
+
+
+
+
+
+
+
+
+
+/*
+//............................................Funcion para "rotar" un arreglo un "n" veces...........................
+
+function rotar (arr, des){
+  for(let i=0; i<des; i++){
+    arr.unshift(arr.pop())
+  }
+  return arr;
+}
+//...................................................................................................................
+//.................................................Funcion para codificar............................................
+function desplazar (str,offset){// la funcion desplazar requiere una cadena y el numero de desplazamientos u offset
+  let desplazado =[];//se crea un arreglo, para ir guardando las letras desplazadas y posteriormente convertirlo en string
+  for (let i = 0; i <str.length; i++){
+    let mIndex = minu.indexOf(str[i]) //busca en el arreglo "minu" la primera letra del str y regresa la posicion en minu
+    let MIndex = mayu.indexOf(str[i]) //busca en el arreglo "mayu" la primera letra del str y regresa la posicion en mayu
+    //console.log(MIndex)
+    //console.log(mIndex)
+    if(mIndex < 0){ // if's anidados para determinar si la letra es mayuscula, minuscula o espacio e ir formando el arreglo
+      if(MIndex < 0){
+        //console.log(" ")
+        desplazado.push(" ")// agrega espacio a desplazado
+      }else {
+        let newPos = (MIndex+offset)%26 //utilizando la formula de la documentacion, se calcula la nueva letra
+        //console.log(mayu[offset])
+        desplazado.push(mayu[newPos])// agrega letra mayuscula
+      }
+    } else {
+    let newPos = (mIndex + offset)%26
+    //console.log(minu[offset])
+    desplazado.push(minu[newPos])// agrega letra minuscula
+    }
+  }
+  //console.log(desplazado)
+  //console.log(desplazado.join(''))
+  let movida = desplazado.join('') // convierte el arreglo desplazado a un str
+  return movida 
+}
+//...................................................................................................................
+
+//.................................................Funcion para decodificar..........................................
+function regresar (str,offset){//La misma funcion que codificar, solo que el offset es de: "26-offset"
+  let desplazado =[];// es quivalente dar una vuelta completa menos el offset, que regresar el offset
+  for (let i = 0; i <str.length; i++){
+    let mIndex = minu.indexOf(str[i]);
+    let MIndex = mayu.indexOf(str[i])
+    //console.log(MIndex)
+    //console.log(mIndex)
+    if(mIndex < 0){
+      if(MIndex < 0){
+        //console.log(" ")
+        desplazado.push(" ")
+      }else {
+        let newPos = (MIndex + 26 - offset)%26 
+        //console.log(mayu[offset])
+        desplazado.push(mayu[newPos])
+      }
+    } else {
+    let newPos = (mIndex + 26 - offset)%26
+    //console.log(minu[offset])
+    desplazado.push(minu[newPos])
+    }
+  }
+  //console.log(desplazado)
+  //console.log(desplazado.join(''))
+  let movida = desplazado.join('')
+  return movida 
+}
+//...................................................................................................................
+//............................................Arreglos necesarios para las funciones desplazar y regresar............
+let minu = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let mayu = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+//...................................................................................................................
+*/
--- src/index.js
+++ src/index.js
@@ -1,90 +1,53 @@
 //cipher.saludar()
 let boton = document.getElementById("encoding")
 boton.addEventListener("click",function (){cipher.saludar()})
-//............................................Funcion para "rotar" un arreglo un "n" veces...........................
 
 
-function rotar (arr, des){
-  for(let i=0; i<des; i++){
-    arr.unshift(arr.pop())
+
+
+function code (offset,str){
+  if(!(typeof(str)==='string')){
+    let noEs = "Error, ingrese una cadena de caracteres"
+    return  noEs
   }
-  return arr;
-}
-//...................................................................................................................
-
-
-console.log("............................")
-let str1 ="Holamudo";
-
-
-//............................................Arreglos necesarios para las funciones desplazar y regresar............
-let minu = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let mayu = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-//...................................................................................................................
-
-
-//.................................................Funcion para codificar............................................
-function desplazar (str,offset){// la funcion desplazar requiere una cadena y el numero de desplazamientos u offset
-  let desplazado =[];//se crea un arreglo, para ir guardando las letras desplazadas y posteriormente convertirlo en string
-  for (let i = 0; i <str.length; i++){
-    let mIndex = minu.indexOf(str[i]) //busca en el arreglo "minu" la primera letra del str y regresa la posicion en minu
-    let MIndex = mayu.indexOf(str[i]) //busca en el arreglo "mayu" la primera letra del str y regresa la posicion en mayu
-    //console.log(MIndex)
-    //console.log(mIndex)
-    if(mIndex < 0){ // if's anidados para determinar si la letra es mayuscula, minuscula o espacio e ir formando el arreglo
-      if(MIndex < 0){
-        //console.log(" ")
-        desplazado.push(" ")// agrega espacio a desplazado
-      }else {
-        let newPos = (MIndex+offset)%26 //utilizando la formula de la documentacion, se calcula la nueva letra
-        //console.log(mayu[offset])
-        desplazado.push(mayu[newPos])// agrega letra mayuscula
-      }
+  let desplazado = ""
+  for(let i = 0; i <str.length; i++){
+    if(str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90){
+      desplazado = desplazado + String.fromCharCode((str.charCodeAt(i)+offset-65)%26+65)
+    } else { if(str.charCodeAt(i)>=97 && str.charCodeAt(i)<=122){
+      desplazado = desplazado + String.fromCharCode((str.charCodeAt(i)+offset-97)%26+97)
     } else {
-    let newPos = (mIndex + offset)%26
-    //console.log(minu[offset])
-    desplazado.push(minu[newPos])// agrega letra minuscula
+      desplazado =desplazado + String.fromCharCode(str.charCodeAt(i))
+    }
+      
     }
   }
-  //console.log(desplazado)
-  //console.log(desplazado.join(''))
-  let movida = desplazado.join('') // convierte el arreglo desplazado a un str
-  return movida 
+  return desplazado
 }
-//...................................................................................................................
 
-console.log(desplazar(str1,2));
-
-
-//.................................................Funcion para decodificar..........................................
-function regresar (str,offset){//La misma funcion que codificar, solo que el offset es de: "26-offset"
-  let desplazado =[];// es quivalente dar una vuelta completa menos el offset, que regresar el offset
-  for (let i = 0; i <str.length; i++){
-    let mIndex = minu.indexOf(str[i]);
-    let MIndex = mayu.indexOf(str[i])
-    //console.log(MIndex)
-    //console.log(mIndex)
-    if(mIndex < 0){
-      if(MIndex < 0){
-        //console.log(" ")
-        desplazado.push(" ")
-      }else {
-        let newPos = (MIndex + 26 - offset)%26 
-        //console.log(mayu[offset])
-        desplazado.push(mayu[newPos])
-      }
+function decode (offset,str){
+  if(!(typeof(str)==='string')){
+    let noEs = "Error, ingrese una cadena de caracteres"
+    return  noEs
+  }
+  let desplazado = ""
+  for(let i = 0; i <str.length; i++){
+    if(str.charCodeAt(i)>=65 && str.charCodeAt(i)<=90){
+      desplazado = desplazado + String.fromCharCode((str.charCodeAt(i)-offset-65 +26)%26+65)
+    } else { if(str.charCodeAt(i)>=97 && str.charCodeAt(i)<=122){
+      desplazado = desplazado + String.fromCharCode((str.charCodeAt(i)-offset-97 +26)%26+97)
     } else {
-    let newPos = (mIndex + 26 - offset)%26
-    //console.log(minu[offset])
-    desplazado.push(minu[newPos])
+      desplazado =desplazado + String.fromCharCode(str.charCodeAt(i))
+    }
+      
     }
   }
-  //console.log(desplazado)
-  //console.log(desplazado.join(''))
-  let movida = desplazado.join('')
-  return movida 
+  return desplazado
 }
-//...................................................................................................................
 
-
-console.log(regresar(desplazar(str1,2),2))
+let str = "HoLa MuDo"
+let x = code(1,str)
+console.log(x)
+let y = decode(1,str)
+console.log(y)
+console.log(decode(1,code(1,str)))
